c109b665bd438bf9e5d6c1db5eb85f2f64161410
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-05-10 11:39:37 +0200

    .

--- Project-Api/app/Providers/AuthServiceProvider.php
+++ Project-Api/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,14 +15,13 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        'App\Models\Model'=> 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
+    
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+        Passport::routes();
     }
 }
