c667e23d7d6c42e89ac776f4774c745c470af7b3
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-05-10 11:57:30 +0200

    created api in auth

--- Project-Api/app/Providers/AuthServiceProvider.php
+++ Project-Api/app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+//use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -19,9 +19,8 @@ class AuthServiceProvider extends ServiceProvider
     ];
 
     
-    public function boot(): void
+    public function boot():void
     {
         $this->registerPolicies();
-        Passport::routes();
     }
 }
