4bf3619cc6b6358f1ad1df895901056ebad7d870
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-05-11 12:38:13 +0200

    created login

--- Project-Api/app/Http/Controllers/UserController.php
+++ Project-Api/app/Http/Controllers/UserController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Validator;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use stdClass;
-
 
 class UserController extends Controller
 {
@@ -48,12 +45,22 @@ class UserController extends Controller
         return response($response, 201);
     }
 
-    public function Login(Request $request)
+    public function login(Request $request)
     {
-        $credentials = [
-            'email'=>$request->email,
-            'password'=>$request->password,
-        ];
+        $login= $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+       
+        $user = User::where('email', $login['email'])->first();
+        
+        if(!Auth::attempt($login)){
+            return response(['message' =>'Invalid login credentials'],401);
+        }else{
+            $accessToken = $user->createToken('authToken')->accessToken;
+        }
+        
+        return response(['user'=> Auth::user(), 'access_token'=> $accessToken]);
     }
 
     /**
