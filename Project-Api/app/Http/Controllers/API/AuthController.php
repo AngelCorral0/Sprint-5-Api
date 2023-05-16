c783de237120c7ee39544225416f645a47b47bde
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-05-16 22:21:33 +0200

    Auth done

--- Project-Api/app/Http/Controllers/API/AuthController.php
+++ Project-Api/app/Http/Controllers/API/AuthController.php
@@ -15,80 +15,55 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        
-       $validatedData = $request->validate([
+        $validatedData = $request->validate([
             'email' => ['required', 'unique:users', 'string', 'email'],
             'username' => ['max:25', 'string'],
             'password' => ['required', 'string']
         ]);
-        if($request->username == null){
-            $request->merge(['username' => 'Anonymous']);
-        }
-       
-
-        $validatedData['password'] = Hash::make($request->password);
-           
+        
+        if($request->username == null||$request->username ==''){
+            $request->merge(['username' => 'Anonymous']); 
+         }
+    
+         $validatedData['password'] = Hash::make($request->password);
         $user = User::create($validatedData);
         $accessToken = $user->createToken('authToken')->accessToken;
-    
+        
+        
         return response([
             'message' => 'User registered',
-            'user' => $user,   
+        'user' => [
+            'username' => $user->username,
+            'email'=>$user->email,
             'access_token' => $accessToken
-        ],201);
+        ],201]);
     }
         
-    
-        //     $user = new User();
-        //     $user->email = $request->email;
-        //     $user->password = Hash::make($request->password);
-        //     // if(!$request->username){
-        //     //     $user = 'Anonymous';
-        //     // }else{
-        //     //     $user->username = $request->usermane;
-        //     // }
-        //     $user->save();
-        //     $response = [
-        //         'user' => $user
-          //  ];
-        
-        
-
     public function login(Request $request)
     {
         $login= $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string'
         ]);
-       
-        $user = User::where('email', $login['email'])->first();
-        
+               
         if(!Auth::attempt($login)){
             return response(['message' =>'Invalid login credentials'],401);
-        }else{
-            $accessToken = $user->createToken('authToken')->accessToken;
         }
-        
-        return response(['user'=> Auth::user(), 'access_token'=> $accessToken]);
+
+         $accessToken = auth()->user->createToken('authToken')->accessToken;
+                
+        return response([
+            'user'=> Auth::user(), 
+            'access_token'=> $accessToken]);
     }
 
-    public function show(User $user)
+    public function logout(Request $request)
     {
-        return response()->json($user);
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ],200);
     }
 
-    public function update(Request $request, User $user)
-    {
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
-        return response()->json($user);
-    }
-
-    public function destroy(User $user)
-    {
-        $user->delete();
-        return response()->json($user);
-    }
 }
