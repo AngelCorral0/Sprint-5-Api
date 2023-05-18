f5165b570b195a39db86768414493d3fb1366c5e
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-05-18 11:50:15 +0200

    creating game controller

--- Project-Api/app/Http/Controllers/API/GameController.php
+++ Project-Api/app/Http/Controllers/API/GameController.php
@@ -2,32 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+       
+    public function diceRoll($id)
+    {  
+       $authUser= Auth::user()->$id;
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $dice1 = random_int(1,6);
-        $dice2 = random_int(1,6);
-        $totalValue = $dice1+$dice2;
+       if($authUser==$id)
+       {        
+        $dice1 = rand(1,6);
+        $dice2 = rand(1,6);
+        $total = $dice1+$dice2;
+        $result = $total== 7? 'You Win!' : 'You Lost';
 
-        if($totalValue == 7){
-            return "You won!";
-        }else{
-            return "Try loss!";
-        }
+        $game = new Game();
+
+        $game->user_id = $id;
+        $game->dice1;
+        $game->dice2;
+        $game->total;
+        $game->result;
+
+        $game->save();
+
+        
+
+       }
+
+       
+
 
     }
 
@@ -37,17 +45,13 @@ class GameController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+   
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+   
     public function destroy(string $id)
     {
         //
