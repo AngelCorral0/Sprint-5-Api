832e84631d43523899b3887871c5c08427e42d8e
AngelCorral0 <angel.corral.olivares@gmail.com>
2023-05-20 12:48:54 +0200

    created api routes

--- Project-Api/routes/api.php
+++ Project-Api/routes/api.php
@@ -6,24 +6,18 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\GameController;
 use App\Http\Controllers\API\UserController;
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
     Route::post('/players', [AuthController::class, 'register'])->name('register');
     Route::post('/players/login', [AuthController::class, 'login'])->name('login');
 
     Route::middleware(['auth:api'])->group(function(){
       
     Route::put('/players/{id}',  [UserController::class, 'editUsername']);
-    //Route::put('/players/{user}', 'UserController@update');
-    //Route::delete('/players/{user}', 'UserController@destroy');
-});
+    Route::post('/players/{id}/games', [GameController::class, 'diceRoll']);
+    Route::delete('/players/{id}/games', [GameController::class, 'destroy']);
+    Route::get('/players', [UserController::class, 'userRanking']);
+    Route::get('/players/{id}/games', [UserController::class, 'userGames']);
+    Route::get('/players/ranking', [GameController::class, 'rankingAverage']);
+    Route::get('/players/ranking/loser', [GameController::class, 'loser']);
+    Route::get('/players/ranking/winner', [GameController::class, 'winner']);
+   
+    });
